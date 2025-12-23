1ff78b04daba1fd14632d3773823f3e89ccdf3f5
Imad Ait Belmouden <imadaitbelmouden8@gmail.com>
2025-12-23 21:28:05 +0100

    first commit

---
+++ microservices-messaging-consumer/src/main/java/microservicesmessagingconsumer/config/RabbitMQConfig.java
@@ -0,0 +1,93 @@
+package microservicesmessagingconsumer.config;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Value("${spring.rabbitmq.queue}")
+    private String queue;
+
+    @Value("${spring.rabbitmq.exchange}")
+    private String exchange;
+
+    @Value("${spring.rabbitmq.routingkey}")
+    private String routingKey;
+
+    @Value("${spring.rabbitmq.username}")
+    private String username;
+
+    @Value("${spring.rabbitmq.password}")
+    private String password;
+
+    @Value("${spring.rabbitmq.host}")
+    private String host;
+
+    @Value("${spring.rabbitmq.port}")
+    private int port;
+
+    @Bean
+    public AmqpAdmin amqpAdmin(ConnectionFactory connectionFactory) {
+        return new RabbitAdmin(connectionFactory);
+    }
+
+    @Bean
+    public Declarables rabbitDeclarables() {
+        Queue queue = new Queue(this.queue, true);
+        DirectExchange exchange = new DirectExchange(this.exchange, true, false);
+        Binding binding = BindingBuilder
+                .bind(queue)
+                .to(exchange)
+                .with(routingKey);
+
+        return new Declarables(queue, exchange, binding);
+    }
+/*
+    @Bean
+    Queue queue() {
+        return new Queue(queue, true);
+    }
+
+    @Bean
+    Exchange myExchange() {
+        return ExchangeBuilder.directExchange(exchange).durable(true).build();
+    }
+
+    @Bean
+    Binding binding() {
+        return BindingBuilder
+                .bind(queue())
+                .to(myExchange())
+                .with(routingKey)
+                .noargs();
+    }
+*/
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        CachingConnectionFactory cf = new CachingConnectionFactory(host, port);
+        cf.setUsername(username);
+        cf.setPassword(password);
+        return cf;
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jsonMessageConverter());
+        return rabbitTemplate;
+    }
+}
---
+++ microservices-messaging-consumer/src/main/java/microservicesmessagingconsumer/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package microservicesmessagingconsumer.repository;
+
+import microservicesmessagingconsumer.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
